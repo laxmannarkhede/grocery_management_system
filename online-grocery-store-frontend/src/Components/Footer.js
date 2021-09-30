bd77a2933c8d6995e9d8b3df2a05a71fa422bd2b
laxmannarkhede <84923177+laxmannarkhede@users.noreply.github.com>
2021-09-30 19:28:14 +0530

    Add files via upload

---
+++ online-grocery-store-frontend/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h6>Developed By G70</h6>
+        </div>
+    )
+}
+
+export default Footer
